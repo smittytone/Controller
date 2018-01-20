543a50ce852d4339a3bf578e36f456daac5a716d
smittytone <tony@electricimp.com>
2018-01-20 14:00:02 +0000

    Add network code, enable switch

--- Controller WatchKit Extension/MatrixClockInterfaceController.swift
+++ Controller WatchKit Extension/MatrixClockInterfaceController.swift
@@ -7,11 +7,18 @@
 import WatchKit
 
 
-class MatrixClockInterfaceController: WKInterfaceController {
+class MatrixClockInterfaceController: WKInterfaceController, URLSessionDataDelegate {
 
     @IBOutlet weak var deviceLabel: WKInterfaceLabel!
+    @IBOutlet weak var statusLabel: WKInterfaceLabel!
     @IBOutlet weak var lightSwitch: WKInterfaceSwitch!
-
+    
+    let deviceBasePath: String = "https://agent.electricimp.com/"
+    var aDevice: Device? = nil
+    var serverSession: URLSession?
+    var connexions: [Connexion] = []
+    var initialQueryFlag: Bool = true
+    
 
     // MARK: - Lifecycle Functions
 
@@ -19,8 +26,18 @@ class MatrixClockInterfaceController: WKInterfaceController {
 
         super.awake(withContext: context)
 
-        let aDevice: Device = context as! Device
-        deviceLabel.setText(aDevice.name)
+        self.aDevice = context as? Device
+        self.deviceLabel.setText(aDevice!.name)
+        self.initialQueryFlag = true
+        self.lightSwitch.setHidden(true)
+    }
+    
+    override func didAppear() {
+        
+        super.didAppear()
+        
+        // Get the device's status
+        makeConnection(nil);
     }
 
     override func willActivate() {
@@ -28,9 +45,12 @@ class MatrixClockInterfaceController: WKInterfaceController {
         super.willActivate()
     }
 
-    @IBAction func doSwitch(_ sender: Any) {
+    @IBAction func doSwitch(value: Bool) {
 
-        // TODO
+        var dict = [String: String]()
+        dict["setlight"] = value ? "1" : "0"
+        lightSwitch.setTitle(value ? "On" : "Off")
+        makeConnection(dict)
     }
 
     @IBAction func back(_ sender: Any) {
@@ -38,5 +58,182 @@ class MatrixClockInterfaceController: WKInterfaceController {
         // Go back to the device list
         popToRootController()
     }
+    
+    
+    // MARK: - Connection Functions
+    
+    func makeConnection(_ data:[String:String]?) {
+        
+        let urlPath :String = deviceBasePath + aDevice!.code + "/settings"
+        let url:URL? = URL(string: urlPath)
+        
+        if url == nil {
+            reportError("MatrixClockInterfaceController.makeConnecion() passed malformed URL string + \(urlPath)")
+            return
+        }
+        
+        if serverSession == nil {
+            serverSession = URLSession(configuration:URLSessionConfiguration.default,
+                                     delegate:self,
+                                     delegateQueue:OperationQueue.main)
+        }
+        
+        var request = URLRequest(url: url!,
+                                 cachePolicy:URLRequest.CachePolicy.reloadIgnoringLocalCacheData,
+                                 timeoutInterval: 60.0)
+        
+        if (data != nil) {
+            do {
+                request.httpBody = try JSONSerialization.data(withJSONObject: data!, options: [])
+                request.httpMethod = "POST"
+            } catch {
+                reportError("MatrixClockInterfaceController.makeConnection() passed malformed data")
+                return
+            }
+        }
+        
+        let aConnexion = Connexion()
+        aConnexion.errorCode = -1;
+        aConnexion.data = NSMutableData.init(capacity:0)
+        aConnexion.task = serverSession!.dataTask(with:request)
+        
+        if let task = aConnexion.task {
+            task.resume()
+            connexions.append(aConnexion)
+        }
+    }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        
+        // This delegate method is called when the server sends some data back
+        // Add the data to the correct connexion object
+        for aConnexion in connexions {
+            // Run through the connections in our list and add the incoming data to the correct one
+            if aConnexion.task == dataTask {
+                if let connData = aConnexion.data {
+                    connData.append(data)
+                }
+            }
+        }
+    }
+    
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+        
+        // This delegate method is called when the server responds to the connection request
+        // Use it to trap certain status codes
+        let rps = response as! HTTPURLResponse
+        let code = rps.statusCode;
+        
+        if code > 399 {
+            // The API has responded with a status code that indicates an error
+            
+            for aConnexion in connexions {
+                // Run through the connections in our list and
+                // add the incoming error code to the correct one
+                if aConnexion.task == dataTask { aConnexion.errorCode = code }
+                
+                if code == 404 {
+                    // Agent is moving for production shift, so delay check
+                    completionHandler(URLSession.ResponseDisposition.cancel)
+                } else {
+                    completionHandler(URLSession.ResponseDisposition.allow)
+                }
+            }
+        } else {
+            completionHandler(URLSession.ResponseDisposition.allow);
+        }
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        
+        // All the data has been supplied by the server in response to a connection -
+        // or an error has been encountered
+        // Parse the data and, according to the connection activity
+        if error != nil {
+            // React to a passed client-side error - most likely a timeout or inability to resolve the URL
+            // Notify the host app
+            reportError("Could not connect to the impCloud")
+            
+            // Terminate the failed connection and remove it from the list of current connections
+            var index = -1
+            
+            for i in 0..<connexions.count {
+                // Run through the connections in the list and find the one that has just finished loading
+                let aConnexion = connexions[i]
+                
+                if aConnexion.task == task {
+                    task.cancel()
+                    index = i
+                }
+            }
+            
+            if index != -1 { connexions.remove(at:index) }
+        } else {
+            // Save the clock state data if the connection succeeds
+            for i in 0..<connexions.count {
+                let aConnexion = connexions[i]
+                
+                if aConnexion.task == task {
+                    if let data = aConnexion.data {
+                        let inString = String(data:data as Data, encoding:String.Encoding.ascii)!
+                        
+                        if inString == "OK" || inString == "Not Found\n" || inString == "No handler" {
+                            return
+                        }
+                        
+                        let dataArray = inString.components(separatedBy:".")
+                        let dis = dataArray[8] as String
+                        self.deviceLabel.setText(dis == "d" ? aDevice!.name + " !" : aDevice!.name)
+                        
+                        if self.initialQueryFlag == true {
+                            // Incoming string looks like this:
+                            //    1.1.1.1.01.1.01.1.d.1
+                            //
+                            // with the values:
+                            //    0. mode (1: 24hr, 0: 12hr)
+                            //    1. bst state
+                            //    2. colon flash
+                            //    3. colon state
+                            //    4. brightness
+                            //    5. world time state
+                            //    6. world time offset (0-24 -> -12 to 12)
+                            //    7. display state
+                            //    8. connection status
+                            //    9. debug status
+                            
+                            let powerString = dataArray[7] as String
+                            
+                            // Set the clock state
+                            if let value = Int(powerString) {
+                                if value == 1 {
+                                    lightSwitch.setOn(true)
+                                    lightSwitch.setTitle("On")
+                                } else {
+                                    lightSwitch.setOn(false)
+                                    lightSwitch.setTitle("Off")
+                                }
+                            }
+                            
+                            self.lightSwitch.setHidden(false)
+                            self.initialQueryFlag = false
+                            self.statusLabel.setHidden(true)
+                        }
+                    }
+                    
+                    // End connection
+                    task.cancel()
+                    connexions.remove(at:i)
+                    break
+                }
+            }
+        }
+    }
+    
+    func reportError(_ message:String)
+    {
+        NSLog(message)
+    }
+
 
 }
