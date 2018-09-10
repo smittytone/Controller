c45d8483351bbb2f67082ecdf36b77a7070509db
smittytone <tony@electricimp.com>
2018-09-10 08:45:44 +0100

    Add deactivation notification

--- Controller WatchKit Extension/ExtensionDelegate.swift
+++ Controller WatchKit Extension/ExtensionDelegate.swift
@@ -16,11 +16,19 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
 
     func applicationDidBecomeActive() {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
+
+
     }
 
     func applicationWillResignActive() {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, etc.
+
+        let nc: NotificationCenter = NotificationCenter.default
+        nc.post(name: NSNotification.Name.init("com.bps.controller.app.deactivate"),
+                object: self,
+                userInfo: nil)
     }
 
     func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
